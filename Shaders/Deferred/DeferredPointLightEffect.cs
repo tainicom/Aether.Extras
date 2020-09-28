8c16347f9b7520df8ca333feb0d0e0ba799d0543
Nick Kastellanos <nkastellanos@gmail.com>
2020-09-28 09:18:35 +0300

    Upgrade Deferred shader to MGFX v9

--- Shaders/Deferred/DeferredPointLightEffect.cs
+++ Shaders/Deferred/DeferredPointLightEffect.cs
@@ -69,7 +69,7 @@ namespace tainicom.Aether.Shaders
             // Detect MG version            
             var version = "";
 #if !XNA
-            version = ".8";
+            version = ".9";
             var mgVersion = GetAssembly(typeof(Effect)).GetName().Version;
             if (mgVersion.Major == 3)
             {
@@ -81,6 +81,8 @@ namespace tainicom.Aether.Shaders
                     version = ".8";
                 if (mgVersion.Minor == 7)
                     version = ".8";
+                if (mgVersion.Minor == 8)
+                    version = ".9";
             }
             name = name + version;
 #endif
